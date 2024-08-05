b407034d6acef38779372d9a2c46a0fde52d983d
Kevin Izevbigie <vinn@kevinize.com>
2024-08-05 13:54:37 +0700

    added blank constructor for allowing EF core migrations later

--- src/Frosty.Domain/Framework/Entity.cs
+++ src/Frosty.Domain/Framework/Entity.cs
@@ -4,10 +4,16 @@ namespace Frosty.Domain.Framework;
 abstract class Entity {
 
     private readonly List<IDomainEvent> domainEvents = new();
-
     public Guid Id { get; init; }
 
     protected Entity(Guid id) {
         Id = id;
+    }
+
+
+    // this will be needed later to allow EF core to run
+    // migrations. It remains blank.
+    protected Entity() {
+
     }
 }
