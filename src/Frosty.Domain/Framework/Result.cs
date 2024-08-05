b4f21544263c0f528e9d5b293aa17d4149220490
Kevin Izevbigie <vinn@kevinize.com>
2024-08-05 18:19:56 +0700

    created success and failure with and without a value #7

--- src/Frosty.Domain/Framework/Result.cs
+++ src/Frosty.Domain/Framework/Result.cs
@@ -9,18 +9,38 @@ public class Result {
     public bool IsFailure => !IsSuccess;
 
     protected Result(bool isSuccess, Error error) {
-
         IsSuccess = isSuccess;
         Error = error;
     }
+
+    // return a success result without a value
+    public static Result Success() {
+        return new Result(true, Error.None);
+    }
+
+    public static Result Failure(Error error) {
+        return new Result(false, error);
+    }
+
+    // Create a new success result object which encapsulates
+    // the result value, if any. The result status and error
+    // object is also encapsulated
+    public static Result<TVal> Success<TVal>(TVal value) {
+        return new Result<TVal>(value, true, Error.None);
+    }
+
+    public static Result<TVal> Failure<TVal>(Error error) {
+        return new Result<TVal>(default, false, error);
+    }
+
 }
 
 public class Result<TVal> : Result {
 
-    public readonly TVal _value;
+    public readonly TVal? _value;
 
-    private Result(
-        TVal value,
+    protected internal Result(
+        TVal? value,
         bool isSuccess,
         Error error) : base(isSuccess, error) {
 
@@ -28,18 +48,8 @@ public class Result<TVal> : Result {
 
     }
 
-    // Create a new success result object which encapsulates
-    // the result value, if any. The result status and error
-    // object is also encapsulated
-    public static Result<TVal> Success(
-        TVal value,
-        bool isSuccess,
-        Error error) {
-        return new Result<TVal>(value, isSuccess, error);
-    }
 
-    // TODO: Add the failure static function
-    // TODO: create the error object with a name and descr.
+
     // TODO: Need to validate against success with error object not none.
     // TODO: Need to be able to return a result object without a value...? For example, for failed results, we only need the error object, not the value
 
