a52bdb67a9075c3ad2c9c1b7a1e09b457cbd61cd
Kevin Izevbigie <vinn@kevinize.com>
2024-08-06 11:02:21 +0700

    clean up and add constructor elements #4

--- src/Frosty.Domain/Record/Record.cs
+++ src/Frosty.Domain/Record/Record.cs
@@ -1,31 +1,38 @@
 
 using Frosty.Domain.Framework;
+
 namespace Frosty.Domain.Record;
 
 public sealed class Record : Entity {
 
     public Record(
         Guid id,
-        LeadStatus ls
+        ContactInfo primaryContact,
+        Website website,
+        LeadStatus leadStatus,
+        DateTime createDate,
+        List<ContactInfo>? secondaryContacts = null
     ) : base(id) {
-
-        LeadStatus = ls;
-
+        PrimaryContact = primaryContact;
+        Website = website;
+        LeadStatus = leadStatus;
+        CreateDate = createDate;
+        SecondaryContacts = secondaryContacts;
     }
 
     public ContactInfo PrimaryContact { get; private set; }
-    public List<ContactInfo> SecondaryContacts { get; private set; }
+    public List<ContactInfo>? SecondaryContacts { get; private set; }
 
     // The website is the unique field as a person can own
     // multiple companies
     public Website Website { get; private set; }
 
-    public string EmailVerifyId { get; private set; }
-    public DateTime EmailVerifyDate { get; private set; }
+    public string? EmailVerifyId { get; private set; }
+    public DateTime? EmailVerifyDate { get; private set; }
 
     public LeadStatus LeadStatus { get; private set; }
 
-    public List<Comment> Comments { get; private set; }
+    public List<Comment>? Comments { get; private set; }
 
     // public string Niche { get; private set; }
     // public string NicheType { get; private set; }
@@ -33,49 +40,19 @@ public sealed class Record : Entity {
     public DateTime CreateDate { get; private set; }
 
     // NOTE: if the verification or website service failed, Reject.
-    public DateTime RejectDate { get; private set; }
-    public List<EmailLog> EmailLogs { get; private set; }
-    public int EmailCounter { get; private set; }
+    public DateTime? RejectDate { get; private set; }
+    public List<EmailLog>? EmailLogs { get; private set; }
+    public int? EmailCounter { get; private set; }
 
 }
 
-// An email log records when and how often an email has been sent
-public sealed record EmailLog {
-
-    public string Name;
-    public DateTime LastDateEmailUtc;
-
-    public EmailLog(DateTime emailDate) {
-        LastDateEmailUtc = emailDate;
-        Name = "Email Sent at " + LastDateEmailUtc;
-    }
-
-}
-
-public sealed record Comment(
-    string Name,
-    string Description);
 
 
-public sealed record Website(string Value);
 
-public sealed record ContactInfo(
-    string Firstname,
-    string Lastname,
-    string? Email = null);
 
-public enum LeadStatus {
-    New = 1,
-    NameFound = 2,
-    EmailFound = 3,
-    EmailVerified = 4,
-    Emailed = 5,
-    Rejected = 6,
-}
-
-// secondary contacts - list of value model
 
 
 // NOTE: Potential Services
 // Email Verification Service - regex testing
 // Website Verification Service - ping website for status code
+// Increase email counter
