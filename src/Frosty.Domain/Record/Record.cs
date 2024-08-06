43fba915db12da46e31f9ee497e7121d8606c6c1
Kevin Izevbigie <vinn@kevinize.com>
2024-08-06 08:00:49 +0700

    created value object plan, and services plan #4

--- src/Frosty.Domain/Record/Record.cs
+++ src/Frosty.Domain/Record/Record.cs
@@ -13,11 +13,14 @@ public sealed class Record : Entity {
     public string Firstname { get; private set; }
     public string Lastname { get; private set; }
     public string Email { get; private set; }
+
+    // The website is the unique field as a person can own
+    // multiple companies
     public string Website { get; private set; }
 
     public string VerifyFileId { get; private set; }
     public string EmailVerifyDate { get; private set; }
-    public string LeadStatus { get; private set; }
+    public LeadStatus LeadStatus { get; private set; }
 
     public string Comments { get; private set; }
     public string SecondaryContacts { get; private set; }
@@ -29,9 +32,27 @@ public sealed class Record : Entity {
     public string EmailLog { get; private set; }
 }
 
+// NOTE: Potential Value Objects
+
+// public sealed record ContactInfo();
+// public sealed record VerificationDetails();
+// public sealed record ContactInfo();
+
+public enum LeadStatus {
+    New = 1,
+    NameFound = 2,
+    EmailFound = 3,
+    EmailVerified = 4,
+    Emailed = 5,
+}
 
 // Contact Details - value object - FN/LN/Email
 // secondary contacts - list of value model
 // Lead status - enum
 // 
 
+
+// NOTE: Potential Services
+
+// Email Verification Service - regex testing
+// Website Verification Service - ping website for status code
