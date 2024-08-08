7d94398e765e6e3abf29201d6217cdcccf982519
Kevin Izevbigie <vinn@kevinize.com>
2024-08-08 14:09:04 +0700

    added new properties for time stamps #9

--- src/Frosty.Domain/EmailPipeline/EmailPipeline.cs
+++ src/Frosty.Domain/EmailPipeline/EmailPipeline.cs
@@ -5,7 +5,13 @@ namespace Frosty.Domain.EmailPipeline;
 
 // Sometimes, in the business, this is called a Email Queue. And sometimes it is called an Email Pipeline. It's simply the records that need to be sent an initial email.
 
+// The reason I picked Pipeline instead of queue is because I want to seperate business thinking from the Queue data Structure.
+
 public class EmailPipeline : Entity {
+
+
+    public DateTime AddedToPipelineUtc { get; private set; }
+    public DateTime RemovedToPipelineUtc { get; private set; }
 
     public List<Record>? EmailQueue { get; private set; }
 
@@ -13,7 +19,6 @@ public class EmailPipeline : Entity {
 
 
     public void Add(Guid recordId) {
-
 
 
     }
