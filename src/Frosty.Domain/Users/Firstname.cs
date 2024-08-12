a2ca580ced3e4202c0e1e95f441a131de5c6a4ec
Kevin Izevbigie <vinn@kevinize.com>
2024-08-12 10:11:27 +0700

    decided on using Static Factories to create value objects with validation inside #17 #20

--- src/Frosty.Domain/Users/Firstname.cs
+++ src/Frosty.Domain/Users/Firstname.cs
@@ -15,8 +15,6 @@ public record Firstname {
 
 // NOTE: Plan for VO
 
-// TODO: First, I need to pick a validation style and stay consistent. Static Factory Create with validation with private const? Or validate inside constructor? Or seperate method within record called validated?
-
 // TODO: Create a NameValueObject<T> class to be inhereted
 // // Name will have a method called MakeProper
 // // Name will have a function called validate.
@@ -33,7 +31,6 @@ public record Firstname {
 // Match the domain part of the email for Records with the domain (Record only)
 // and therefore, only in the Record Email Value Object
 
-
 // TODO: ContactInfo has strings for all internal fields. Create FN, LN and Email Value Objects for Record.
 
 // TODO: Website Value object in record needs a validate function
@@ -47,4 +44,6 @@ public record Firstname {
 // Comment value should be a 
 
 // TODO: Then, check the other objects. Deal with these first.
-// TODO: 
+
+
+// TODO: Next, sit trhou
