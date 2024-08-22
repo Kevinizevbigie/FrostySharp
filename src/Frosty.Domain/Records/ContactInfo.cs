5f452b54ef0c75f7a8a369ed69c555854a3f0b1c
Kevin Izevbigie <vinn@kevinize.com>
2024-08-22 13:46:24 +0700

    updated format of Contact info

--- src/Frosty.Domain/Records/ContactInfo.cs
+++ src/Frosty.Domain/Records/ContactInfo.cs
@@ -2,7 +2,15 @@
 using Frosty.Domain.Shared;
 namespace Frosty.Domain.Records;
 
-public sealed record ContactInfo(
-    Name<Firstname> Firstname,
-    Name<Lastname> Lastname,
-    Email? Email = null);
+public sealed record ContactInfo {
+    public Name<Firstname> Firstname { get; set; }
+    public Name<Lastname> Lastname { get; set; }
+    public Email? Email = null;
+
+    public ContactInfo(Name<Firstname> fn,
+                       Name<Lastname> ln) {
+
+        Firstname = fn;
+        Lastname = ln;
+    }
+}
