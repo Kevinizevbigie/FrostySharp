16df815327573271407bde13fa815aedb7bb0482
Kevin Izevbigie <vinn@kevinize.com>
2024-08-25 20:13:31 +0700

    added test 12, fixed test 11. #27

--- test/Frosty.Domain.UnitTests/EmailPipelineCard/CardTests.cs
+++ test/Frosty.Domain.UnitTests/EmailPipelineCard/CardTests.cs
@@ -1,6 +1,7 @@
 
 using Frosty.Domain.EmailPipelineCards;
 using Frosty.Domain.EmailPipelineCards.Events;
+using Frosty.Domain.Framework;
 using Frosty.Domain.Records;
 using Frosty.Domain.UnitTests.Records;
 
@@ -13,8 +14,33 @@ public class CardTests {
 
         var record = await CardData.MakeRecord();
 
+
+        await record.VerifyEmailGuesses(RecordServices.VerifyPass);
+
+        // add dummy email email to record
+        record.AddEmail(RecordSensitiveData.EmailAddress);
+
         // change lead status to an incorrect one
         record.ChangeLeadStatus(LeadStatus.WebsiteValid);
+
+        var Card = EmailPipelineCard.Create(
+            record.PrimaryContact.Email?.Value,
+            record
+        );
+
+        var want = CardErrors.CannotAddToSendList;
+        var got = Card.Error;
+
+        Assert.Equal(want, got);
+
+    }
+
+    [Fact]
+    public async void CreateCard_Should_ReturnSuccess() {
+
+        var record = await CardData.MakeRecord();
+
+        await record.VerifyEmailGuesses(RecordServices.VerifyPass);
 
         // add email to record
         record.AddEmail(RecordSensitiveData.EmailAddress);
@@ -24,7 +50,7 @@ public class CardTests {
             record
         );
 
-        var want = CardErrors.CannotAddToSendList;
+        var want = Error.None;
         var got = Card.Error;
 
         Assert.Equal(want, got);
